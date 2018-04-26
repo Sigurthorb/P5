ba7566e97674a534fe48f48d459451628b22f421
Sigurthor Bjorgvinsson <sbjorgvinsson@fc-md.umd.edu>
2018-04-25 21:41:12 -0400

    changed package name, added join port to docker, updated runner.js to start server and client

--- docker/runner.js
+++ docker/runner.js
@@ -1,8 +1,66 @@
+var P5 = require('../index');
 
-console.log("Started as " + (process.env.ROOT_NODE == "TRUE" ? "ROOT": "NON_ROOT"));
-let timer = process.env.WAIT_CONN;
+let isRoot = process.env.ROOT_NODE === "TRUE";
+let waitTime = parseInt(process.env.WAIT_CONN);
 
-let slep = parseInt(timer) * 1000;
+
+var opts = {
+  sendPort:parseInt(process.env.SEND_PORT),
+  receivePort:parseInt(process.env.RECEIVE_PORT),
+  joinPort:parseInt(process.env.JOIN_PORT)
+};
+
+console.log("Starting as " + (process.env.ROOT_NODE == "TRUE" ? "ROOT": "NON_ROOT") + " in " + waitTime + " seconds");
+
 setTimeout(function() {
-    console.log("Now starting after " + timer + "sek wait");
-}, slep);
+    if(isRoot) {
+      startRoot();
+    } else {
+      startClient();
+    }
+}, waitTime*1000);
+
+let startRoot = function() {
+  P5.create(["p5-topology.herokuapp.com"], opts).then(p5server => {
+    var server = p5server;
+  
+    console.log("Got your server.");
+    console.log("This is your public key: ", server.key);
+    console.log("This is your channel:", server.channel);
+  
+    server.on("message", (msg) => {
+      console.log("Message Received\n");
+      console.log(msg);
+    });
+  
+    server.start();
+  }).catch(err => {
+    console.log("Could not create server...");
+    console.log(err);
+  });
+};
+
+
+
+let startClient = function() {
+
+  
+  P5.join("p5-root", "3001", 0, 100, opts).then(p5server => {
+    var server = p5server;
+  
+    console.log("Got your server.");
+    console.log("This is your public key: ", server.key);
+    console.log("This is your channel:", server.channel);
+  
+    server.on("message", (msg) => {
+      console.log("Message Received\n");
+      console.log(msg);
+    });
+  
+    server.start();
+  
+  }).catch(err => {
+    console.log("Could not create server...");
+    console.log(err);
+  });
+}
